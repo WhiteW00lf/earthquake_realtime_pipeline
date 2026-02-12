eab351043cb8678d01c0f001e2462476852f966c
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-12 14:35:46 +0530

    fixed waiting for the publisher to publish the event

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ from google.cloud import pubsub_v1
 import requests
 import json
 
-URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2026-02-12&orderby=time&limit=20'
+URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2026-02-12&orderby=time&limit=100'
 
 r = requests.get(URL)
-topic = 'projects/deportfolio-486507/topics/earthquake-topic'
-subs = 'projects/deportfolio-486507/subscriptions/earthquake_sub'
-#print(r.json())
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path('deportfolio-486507','earthquake-topic')
 
 data = r.json()
 for feature in data['features']:
     place = feature['properties']['place']
     mag = feature['properties']['mag']
     coord = feature['geometry']['coordinates']
-    print(f'Magnitude: {mag}, Place: {place}, Coordinates: {coord}')
+    #print(f'Magnitude: {mag}, Place: {place}, Coordinates: {coord}')
     event = {
         'place': place,
         'magnitude': mag,
@@ -22,9 +21,11 @@ for feature in data['features']:
     }
 
     # Publish the event to Pub/Sub
-    publisher = pubsub_v1.PublisherClient(topic, subs, json.dumps(event).encode('utf-8'))
+    future = publisher.publish(topic_path, json.dumps(event).encode('utf-8'))
+    future.result()  # Wait for the publish to complete
+    print(f"Published message: {event}")
 
-    
+
 
 
     
